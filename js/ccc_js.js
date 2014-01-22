d090cfc2d61ab42173c8c36df9df3cab0e9f0616
Geoff Thierman <geoff.thierman@goclio.com>
2014-01-21 21:08:46 -0800

    The last workshop and speaker-box containers no longer float right if the length of the speakers or workshops is not a multiple of 4

--- js/ccc_js.js
+++ js/ccc_js.js
@@ -1,3 +1,13 @@
 $(function(){
 	$('html').addClass('page-load-fade-in')
+	//Don't float the last .speaker-box right if the number of .speaker-boxes is not a multiple of four.
+  speaker_length = $('.speaker-box').length;
+  if (speaker_length % 4 != 0) {
+    $("#speakers").addClass('dont-float-last')
+  }
+	//Don't float the last .workshop right if the number of .workshops is not a multiple of four.
+  workshop_length = $('.workshop').length;
+  if (workshop_length % 4 != 0) {
+    $("#workshops").addClass('dont-float-last');
+  }
 });
